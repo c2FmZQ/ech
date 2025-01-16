38004f4e347da2afbe4eeaf86073a9a567c689b7
Robin Thellend <rthellend@rthellend.com>
2025-01-16 12:00:43 -0800

    Remove dead code, minor fix (#38)

--- resolve_test.go
+++ resolve_test.go
@@ -1,20 +1,18 @@
 package ech
 
 import (
-	"io"
 	"net"
-	"net/http"
-	"net/http/httptest"
 	"net/url"
 	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/c2FmZQ/ech/dns"
+	"github.com/c2FmZQ/ech/testutil"
 )
 
 func TestResolve(t *testing.T) {
-	ts := startTestDNSServer(t, []dns.RR{
+	ts := testutil.StartTestDNSServer(t, []dns.RR{
 		// example.com A 192.168.0.1
 		//               192.168.0.2
 		{
@@ -215,37 +213,4 @@ func TestResolveResultTargets(t *testing.T) {
 			t.Errorf("[%d] Got %#v, want %#v", i, got, tc.want)
 		}
 	}
-}
-
-func startTestDNSServer(t *testing.T, db []dns.RR) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		defer req.Body.Close()
-		body, _ := io.ReadAll(req.Body)
-		qq, err := dns.DecodeMessage(body)
-		if err != nil {
-			t.Errorf("dns.DecodeMessage: %v", err)
-			return
-		}
-		qq.QR = 1
-		want := qq.Question[0].Name
-		for i := 0; i < len(db); i++ {
-			rr := db[i]
-			if want != rr.Name {
-				continue
-			}
-			if rr.Type == 5 { // CNAME
-				qq.Answer = append(qq.Answer, rr)
-				want = rr.Data.(string)
-				i = -1
-				continue
-			}
-			if qq.Question[0].Type == rr.Type {
-				qq.Answer = append(qq.Answer, rr)
-				continue
-			}
-		}
-		t.Logf("QQ %#v", qq.Question)
-		t.Logf("AA %#v", qq.Answer)
-		w.Write(qq.Bytes())
-	}))
 }
